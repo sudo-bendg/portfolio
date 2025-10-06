bc947d4496abf5d8cc4fb48a8458156377345a41
Benjamin Godfrey <bendg@disroot.org>
2025-10-06 17:59:01 +0100

    feat: add commandHandler

--- scripts/displayMessages.js
+++ scripts/displayMessages.js
@@ -12,6 +12,21 @@ addMessage(messages.welcomeMessage);
 document.getElementById('terminalInput').addEventListener('keydown', (event) => {
   if (event.key === 'Enter') {
     addMessage(document.getElementById('terminalInput').value);
+    commandHandler(document.getElementById('terminalInput').value);
     document.getElementById('terminalInput').value = '';
   }
 })
+
+// Command handler
+const commandHandler = (input) => {
+  const command = input.split(" ")[0];
+  const args = input.split(" ").splice(1);
+
+  switch (command) {
+    case '?':
+      addMessage('help');
+      break;
+    default:
+      addMessage(`command: "${command}" not found. enter '?' for help.`)
+  }
+}
