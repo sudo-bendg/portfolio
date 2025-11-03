ef4aa3f2778de5d906e8e6dee9c670493f55c8d4
Benjamin Godfrey <bendg@disroot.org>
2025-11-03 20:55:45 +0000

    feat: inital home  page content

--- scripts/homePage.js
+++ scripts/homePage.js
@@ -3,8 +3,6 @@ const pageTitleAscii = "                                        \n ▄▄       
 let pageTitle = document.getElementById('pageTitle');
 
 if (window.innerHeight > 500 && window.innerWidth > 700) {
-    console.log("hi")
-    // insert a readonly, non-resizable textarea so the ASCII art is fixed
     pageTitle.innerHTML = `<textarea readonly spellcheck="false" tabindex="-1">${pageTitleAscii}</textarea>`;
 } else {
     pageTitle.innerHTML = "<h2>home</h2>";
